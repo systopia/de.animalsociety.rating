61ce3be1b9e0095cb7f85daec39ca6f3060d7d4f
Anna <bugey@systopia.de>
2021-11-09 16:32:20 +0100

    added upgrader

---
+++ CRM/Rating/Upgrader.php
@@ -0,0 +1,52 @@
+<?php
+use CRM_Rating_ExtensionUtil as E;
+
+/**
+ * Collection of upgrade steps.
+ */
+class CRM_Rating_Upgrader extends CRM_Rating_Upgrader_Base {
+
+  /**
+   * Example: Run an external SQL script when the module is installed.
+   */
+  public function install() {
+    // install remote event stuff
+    $customData = new CRM_Rating_CustomData(E::LONG_NAME);
+    $customData->syncOptionGroup(E::path('resources/option_group_activity_category.json'));
+
+  }
+
+  /**
+   * Example: Work with entities usually not available during the install step.
+   *
+   * This method can be used for any post-install tasks. For example, if a step
+   * of your installation depends on accessing an entity that is itself
+   * created during the installation (e.g., a setting or a managed entity), do
+   * so here to avoid order of operation problems.
+   */
+  // public function postInstall() {
+  //  $customFieldId = civicrm_api3('CustomField', 'getvalue', array(
+  //    'return' => array("id"),
+  //    'name' => "customFieldCreatedViaManagedHook",
+  //  ));
+  //  civicrm_api3('Setting', 'create', array(
+  //    'myWeirdFieldSetting' => array('id' => $customFieldId, 'weirdness' => 1),
+  //  ));
+  // }
+
+
+
+  /**
+   * Example: Run a couple simple queries.
+   *
+   * @return TRUE on success
+   * @throws Exception
+   */
+  // public function upgrade_0010(): bool {
+  //   $this->ctx->log->info('Applying update 0010');
+  //   CRM_Core_DAO::executeQuery('UPDATE foo SET bar = "whiz"');
+  //   CRM_Core_DAO::executeQuery('DELETE FROM bang WHERE willy = wonka(2)');
+  //   return TRUE;
+  // }
+
+}
