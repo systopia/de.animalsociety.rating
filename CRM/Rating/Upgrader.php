ac8288fc4dd714843bb21be24758865b9b648d3e
Anna <bugey@systopia.de>
2021-11-10 17:57:03 +0100

    added contact custom fields

--- CRM/Rating/Upgrader.php
+++ CRM/Rating/Upgrader.php
@@ -10,9 +10,14 @@ class CRM_Rating_Upgrader extends CRM_Rating_Upgrader_Base {
    * Example: Run an external SQL script when the module is installed.
    */
   public function install() {
-    // install remote event stuff
+    // install option groups and custom groups
     $customData = new CRM_Rating_CustomData(E::LONG_NAME);
     $customData->syncOptionGroup(E::path('resources/option_group_activity_category.json'));
+    $customData->syncOptionGroup(E::path('resources/option_group_activity_score.json'));
+    $customData->syncOptionGroup(E::path('resources/option_group_activity_theme.json'));
+    $customData->syncOptionGroup(E::path('resources/option_group_activity_weight.json'));
+    $customData->syncCustomGroup(E::path('resources/custom_group_activity_factors.json'));
+    $customData->syncCustomGroup(E::path('resources/custom_group_contact_results.json'));
 
   }
 
