f6271cb766539633994a806b8af95ac20fbc85d9
Anna <bugey@systopia.de>
2021-11-17 09:55:16 +0100

    fixed option groups

--- CRM/Rating/Upgrader.php
+++ CRM/Rating/Upgrader.php
@@ -12,10 +12,11 @@ class CRM_Rating_Upgrader extends CRM_Rating_Upgrader_Base {
   public function install() {
     // install option groups and custom groups
     $customData = new CRM_Rating_CustomData(E::LONG_NAME);
-    $customData->syncOptionGroup(E::path('resources/option_group_activity_category.json'));
+    $customData->syncOptionGroup(E::path('resources/option_group_activity_class.json'));
     $customData->syncOptionGroup(E::path('resources/option_group_activity_score.json'));
     $customData->syncOptionGroup(E::path('resources/option_group_activity_theme.json'));
     $customData->syncOptionGroup(E::path('resources/option_group_activity_weight.json'));
+    $customData->syncOptionGroup(E::path('resources/option_group_activity_type.json'));
     $customData->syncCustomGroup(E::path('resources/custom_group_activity_factors.json'));
     $customData->syncCustomGroup(E::path('resources/custom_group_contact_results.json'));
 
