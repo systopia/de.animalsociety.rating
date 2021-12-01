f606d446060b7c67f2c84c0694326c7a68b73d9d
Anna <bugey@systopia.de>
2021-12-01 17:51:02 +0100

    fixed custom fields

--- CRM/Rating/Upgrader.php
+++ CRM/Rating/Upgrader.php
@@ -27,16 +27,18 @@ class CRM_Rating_Upgrader extends CRM_Rating_Upgrader_Base {
   public function install() {
     // install option groups and custom groups
     $customData = new CRM_Rating_CustomData(E::LONG_NAME);
-    $customData->syncOptionGroup(E::path('resources/option_group_activity_class.json'));
-    $customData->syncOptionGroup(E::path('resources/option_group_activity_score.json'));
     $customData->syncOptionGroup(E::path('resources/option_group_activity_category.json'));
+    $customData->syncOptionGroup(E::path('resources/option_group_activity_kind.json'));
+    $customData->syncOptionGroup(E::path('resources/option_group_activity_score.json'));
+    $customData->syncOptionGroup(E::path('resources/option_group_activity_species.json'));
     $customData->syncOptionGroup(E::path('resources/option_group_activity_subcategory.json'));
-    $customData->syncOptionGroup(E::path('resources/option_group_activity_weight.json'));
     $customData->syncOptionGroup(E::path('resources/option_group_activity_type.json'));
     $customData->syncOptionGroup(E::path('resources/option_group_activity_vote.json'));
+    $customData->syncOptionGroup(E::path('resources/option_group_activity_weight.json'));
+    $customData->syncOptionGroup(E::path('resources/option_group_contact_importance.json'));
     $customData->syncCustomGroup(E::path('resources/custom_group_political_activity_additional_fields.json'));
     $customData->syncCustomGroup(E::path('resources/custom_group_contact_results.json'));
-
+    $customData->syncCustomGroup(E::path('resources/custom_group_contact_help_fields.json'));
   }
 
   /**
