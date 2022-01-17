31f714ca50eac8b5246144c8423857df0e6450d0
Philipp Batroff <batroff@systopia.de>
2022-01-17 15:03:50 +0100

    Some refactoring and excercise with anna
    First implementation and start of rating Calculations

--- CRM/Rating/Algorithm.php
+++ CRM/Rating/Algorithm.php
@@ -20,7 +20,7 @@ use CRM_Rating_ExtensionUtil as E;
 /**
  * This class implements the algorithms to calculate the aggregated rating values
  */
-class CRM_Rating_Algorithm
+    class CRM_Rating_Algorithm
 {
     /** @var string custom field key for the activity type */
     const ACTIVITY_TYPE = 'political_activity';
@@ -120,6 +120,28 @@ class CRM_Rating_Algorithm
     /** @var string custom field key for the food_consumer_protection coefficents */
     const FOOD_CONSUMER_PROTECTION_COEFFICENTS = 'contact_help_fields.food_consumer_protection_coefficents';
 
+    /** @var array @var list of activity kind weights */
+    const ACTIVITY_KIND_MAPPING = [
+        1 => 1, // public communication
+        2 => 2, // speech
+    ];
+
+    /** @var array Mapping for weight properties */
+    const ACTIVITY_WEIGHT_MAPPING = [
+        1 => 1,
+        1.5 => 1.5,
+        2 => 2,
+        2.5 => 2.5,
+        3 => 3
+    ];
+
+    const ACTIVITY_SCORE_MAPPING = [
+        0 => 0,     // bad
+        3 => 3,     // rather bad
+        7 => 7,     // rather good
+        10 => 10,   // good
+    ];
+
 
     /** @var array the list of fields that are relevant for the calculations */
     const RELEVANT_CONTACT_FIELDS = [
@@ -189,31 +211,31 @@ class CRM_Rating_Algorithm
     public static function updateContact(int $contact_id)
     {
         // step 1: fetch the contact data
-        $current_data_query = [
-            'id' => $contact_id,
-            'return' => self::getResolvedFieldList(self::RELEVANT_CONTACT_FIELDS)
-        ];
-        $current_data = civicrm_api3('Contact', 'getsingle', $current_data_query);
-        CRM_Rating_CustomData::labelCustomFields($current_data);
+        $current_data = self::fetch_contact_data($contact_id);
 
         // step 2: fetch all relevant activities
-        $activities = civicrm_api3('Activity', 'get', [
-            'option.limit' => 0,
-            'activity_type_id' => self::ACTIVITY_TYPE,
-            'status_id' => self::ACTIVITY_STATUS,
-            'target_contact_id' => $contact_id,
-            'return' => self::getResolvedFieldList(self::RELEVANT_ACTIVITY_FIELDS)
-        ])['values'];
+        $activities = self::fetch_relevant_activities($contact_id);
+
 
         // step 3: get current date & recalculate values
         $current_date = date('y-m-d h:i:s');
         $example_calculated_score = 0.0;
         foreach ($activities as $activity) {
+
+            $activity_type_coefficient = '';
+
             CRM_Rating_CustomData::labelCustomFields($activity);
-            $time_interval = $activity.activity_date_time->diff($current_date);
-            $f_temp_gewichtung = 0.75 / (((($time_interval)/365)/2.9)^4 + 1) + .25;
+            $activity_kind_coefficient = self::ACTIVITY_KIND_MAPPING[$activity[self::ACTIVITY_KIND]];
+            $activity_weight_coefficient = self::ACTIVITY_WEIGHT_MAPPING[$activity[self::ACTIVITY_WEIGHT]];
+            $activity_score = self::ACTIVITY_SCORE_MAPPING[$activity[self::ACTIVITY_SCORE]];
+
+            $activity_rating_weighted = $activity_kind_coefficient * $activity_weight_coefficient * $activity_score;
+            self::write_rating_weighted($activity['id'], $activity_rating_weighted );
+
+            //            $time_interval = $activity.activity_date_time->diff($current_date);
+//            $f_temp_gewichtung = 0.75 / (((($time_interval)/365)/2.9)^4 + 1) + .25;
             //hier muss eigentlich noch ein mapping hin für kind, weight und score:
-            self::ACTIVITY_RATING_WEIGHTED = self::ACTIVITY_KIND * self::ACTIVITY_SCORE * self::ACTIVITY_WEIGHT * $f_temp_gewichtung;
+//            $tmp = self::ACTIVITY_KIND * self::ACTIVITY_SCORE * self::ACTIVITY_WEIGHT * $f_temp_gewichtung;
 
             //$example_calculated_score += $activity[self::ACTIVITY_RATING_WEIGHTED]; ?
         }
@@ -231,6 +253,56 @@ class CRM_Rating_Algorithm
        } else {
             Civi::log()->debug("Contact [{$contact_id}] NOT updated (score hasn't changed).");
         }
+    }
+
+    ///////////////////////////////////////////////////////
+    // Helper functions
+
+    /**
+     * Fetches contact_data with needed rating parameters
+     *
+     * @param $contact_id
+     *
+     * @return array
+     * @throws \CiviCRM_API3_Exception
+     */
+    public static function fetch_contact_data($contact_id) {
+        $current_data_query = [
+            'id' => $contact_id,
+            'return' => self::getResolvedFieldList(self::RELEVANT_CONTACT_FIELDS)
+        ];
+        $current_data = civicrm_api3('Contact', 'getsingle', $current_data_query);
+        CRM_Rating_CustomData::labelCustomFields($current_data);
+        return $current_data;
+    }
+
+
+    public static function write_rating_weighted($activity_id, $rating_weighted) {
+        $result = civicrm_api3('Activity', 'create', [
+            'id' => $activity_id,
+            self::ACTIVITY_RATING_WEIGHTED => $rating_weighted,
+        ]);
+    }
+
+
+    /**
+     * fetches the relevant activities to given contact with specified
+     * return values (self::RELEVANT_ACTIVITY_FIELDS)
+     *
+     * @param $contact_id
+     *
+     * @return mixed
+     * @throws \CiviCRM_API3_Exception
+     */
+    public static function fetch_relevant_activities($contact_id) {
+        $activities = civicrm_api3('Activity', 'get', [
+            'option.limit' => 0,
+            'activity_type_id' => self::ACTIVITY_TYPE,
+            'status_id' => self::ACTIVITY_STATUS,
+            'target_contact_id' => $contact_id,
+            'return' => self::getResolvedFieldList(self::RELEVANT_ACTIVITY_FIELDS)
+        ]);
+        return $activities['values'];
     }
 
     /**
