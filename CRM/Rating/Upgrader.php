5f42e29af1749c8e689a84577907df471e2307a7
B. Endres <endres@systopia.de>
2022-04-27 13:00:40 +0200

    [#17996] added CTA Facebook field

--- CRM/Rating/Upgrader.php
+++ CRM/Rating/Upgrader.php
@@ -45,38 +45,27 @@ class CRM_Rating_Upgrader extends CRM_Rating_Upgrader_Base
     }
 
     /**
-     * Example: Work with entities usually not available during the install step.
+     * Run first upgrader: redo all fields for l10n
      *
-     * This method can be used for any post-install tasks. For example, if a step
-     * of your installation depends on accessing an entity that is itself
-     * created during the installation (e.g., a setting or a managed entity), do
-     * so here to avoid order of operation problems.
+     * @return TRUE on success
+     * @throws Exception
      */
-    // public function postInstall() {
-    //  $customFieldId = civicrm_api3('CustomField', 'getvalue', array(
-    //    'return' => array("id"),
-    //    'name' => "customFieldCreatedViaManagedHook",
-    //  ));
-    //  civicrm_api3('Setting', 'create', array(
-    //    'myWeirdFieldSetting' => array('id' => $customFieldId, 'weirdness' => 1),
-    //  ));
-    // }
-
-
-//    /**
-//     * Example: Run a couple simple queries.
-//     *
-//     * @return TRUE on success
-//     * @throws Exception
-//     */
-//    public function upgrade_0002()
-//    {
-//        $this->ctx->log->info('Updating data structures');
-//        $customData = new CRM_Rating_CustomData(E::LONG_NAME);
-//        $customData->syncCustomGroup(E::path('resources/custom_group_contact_help_fields.json'));
-//        $customData->syncCustomGroup(E::path('resources/custom_group_contact_results.json'));
-//        $customData->syncCustomGroup(E::path('resources/custom_group_political_activity_additional_fields.json'));
-//        return true;
-//    }
-
+    public function upgrade_0001()
+    {
+        $this->ctx->log->info('Translating data structures');
+        $customData = new CRM_Rating_CustomData(E::LONG_NAME);
+        $customData->syncOptionGroup(E::path('resources/option_group_activity_category.json'));
+        $customData->syncOptionGroup(E::path('resources/option_group_activity_kind.json'));
+        $customData->syncOptionGroup(E::path('resources/option_group_activity_score.json'));
+        $customData->syncOptionGroup(E::path('resources/option_group_activity_species.json'));
+        $customData->syncOptionGroup(E::path('resources/option_group_activity_status.json'));
+        $customData->syncOptionGroup(E::path('resources/option_group_activity_subcategory.json'));
+        $customData->syncOptionGroup(E::path('resources/option_group_activity_type.json'));
+        $customData->syncOptionGroup(E::path('resources/option_group_activity_vote.json'));
+        $customData->syncOptionGroup(E::path('resources/option_group_activity_weight.json'));
+        $customData->syncOptionGroup(E::path('resources/option_group_contact_importance.json'));
+        $customData->syncCustomGroup(E::path('resources/custom_group_political_activity_additional_fields.json'));
+        $customData->syncCustomGroup(E::path('resources/custom_group_contact_results.json'));
+        return true;
+    }
 }
